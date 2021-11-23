77abe73d6babf1427ee8223783d9a3668ce18515
navarro <navarro571g@gmial.com>
2021-11-23 17:47:19 -0300

    [UPDATE] routes

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "bundle.js",
-        publicPath: "/"
+        publicPath: "/task-frontend/"
     },
     resolve: {
         extensions: ['.js', '.jsx'],
