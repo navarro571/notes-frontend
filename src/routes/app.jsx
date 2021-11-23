889b31e2eb554af44e02bf51a15d1f4d694041fc
navarro <navarro571g@gmial.com>
2021-11-23 18:08:35 -0300

    [UPDATE] routes

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Home from "@pages/home.jsx";
+import NotFound from "@pages/NotFound.jsx";
 import AppContext from "@context/AppContext";
 
 const App = () => {
@@ -14,7 +15,8 @@ const App = () => {
         <BrowserRouter>
             <AppContext.Provider value={ {taskgroups: taskgroups, setTaskGroups: setTaskGroups } }>
                 <Routes>
-                    <Route exact path="/task-frontend/" element={<Home/>}/>
+                    <Route exact path="task-frontend/" element={<Home/>}/>
+                    <Route path="task-frontend/*" element={<NotFound/>}/>
                 </Routes>
             </AppContext.Provider>
         </BrowserRouter>
