cb9b1eaf6e96c9c52be08b873d9310529070f83d
navarro <navarro571g@gmial.com>
2021-11-23 18:01:08 -0300

    [UPDATE] routes

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Home from "@pages/home.jsx";
-import NotFound from "@pages/NotFound.jsx";
 import AppContext from "@context/AppContext";
 
 const App = () => {
@@ -15,7 +14,7 @@ const App = () => {
         <BrowserRouter>
             <AppContext.Provider value={ {taskgroups: taskgroups, setTaskGroups: setTaskGroups } }>
                 <Routes>
-                    <Route exact path="/" element={<Home/>}/>
+                    <Route exact path="task-frontend/" element={<Home/>}/>
                 </Routes>
             </AppContext.Provider>
         </BrowserRouter>
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "bundle.js",
-        publicPath: "/task-frontend/"
+        publicPath: "/"
     },
     resolve: {
         extensions: ['.js', '.jsx'],
